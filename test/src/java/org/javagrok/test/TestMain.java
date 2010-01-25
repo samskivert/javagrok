842c0fe845b9fdf36b4e557fea04a3a5cbba1656
mdb <mdb@bering.cs.washington.edu>
2010-01-25 15:17:44 -0800

    A simple test program on which to run analyses and JavaGrok.

---
+++ test/src/java/org/javagrok/test/TestMain.java
@@ -0,0 +1,40 @@
+//
+// $Id$
+
+package org.javagrok.test;
+
+/**
+ * A simple test program on which to test analyses and JavaGrok.
+ */
+public class TestMain
+{
+    /** A little box, that holds an integer. */
+    public static class IntBox {
+        /** The initial value stored in the box. */
+        public int initialValue;
+
+        public IntBox (int value) {
+            this.initialValue = value;
+            _currentValue = value;
+        }
+
+        /** Returns the current value of our integer. */
+        public int current () {
+            return _currentValue;
+        }
+
+        /** Increments the boxed integer by the specified amount. */
+        public void increment (int amount) {
+            _currentValue += amount;
+        }
+
+        private int _currentValue;
+    }
+
+    public static void main (String[] args)
+    {
+        IntBox box = new IntBox(5);
+        box.increment(3);
+        System.out.println("Value " + box.current());
+    }
+}
