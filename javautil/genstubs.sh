9feb951ced911524f2da221b4a8ed24f934ae989
mdb <mdb@bering.cs.washington.edu>
2010-02-16 15:22:15 -0800

    This shouldn't have been committed.

--- javautil/genstubs.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-ant javarifier |\
-    grep Stub | grep -v jtest | grep -v javagrok |\
-    awk '{ print $3 }' | sed 's:\.:/:g' | sort |\
-    sed 's:$:.java:' > ~/research/checker-framework/checkers/jdk/javari/stubs.txt
-cd ~/research/checker-framework/checkers/jdk/javari/src
-find `cat ../stubs.txt ` -empty
