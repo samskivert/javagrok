bf6416d494bb5ef7b01d1376ae0d67b497d614c5
mdb <mdb@cs.washington.edu>
2010-02-19 13:58:18 -0800

    Added back and forth movement.

--- experiment/src/experiment/Task1.java
+++ experiment/src/experiment/Task1.java
@@ -4,10 +4,13 @@
 package experiment;
 
 import com.threerings.media.MediaPanel;
-import com.threerings.media.util.LinePath;
-import com.threerings.media.sprite.ImageSprite;
 import com.threerings.media.image.ImageManager;
 import com.threerings.media.image.Mirage;
+import com.threerings.media.sprite.ImageSprite;
+import com.threerings.media.sprite.PathAdapter;
+import com.threerings.media.sprite.Sprite;
+import com.threerings.media.util.LinePath;
+import com.threerings.media.util.Path;
 
 /**
  * Displays a simple sprite on the screen, moving back and forth.
@@ -20,7 +23,17 @@ public class Task1 extends Task
         ImageSprite sprite = new ImageSprite(cartman);
         panel.getSpriteManager().addSprite(sprite);
 
-        int y = (panel.getHeight()-sprite.getHeight())/2;
-        sprite.move(new LinePath(0, y, panel.getWidth()-sprite.getWidth(), y, 1000));
+        final int y = (panel.getHeight()-sprite.getHeight())/2;
+        final int lx = 0, rx = panel.getWidth()-sprite.getWidth();
+        sprite.addSpriteObserver(new PathAdapter() {
+            public void pathCompleted (Sprite sprite, Path path, long when) {
+                if (sprite.getX() == lx) {
+                    sprite.move(new LinePath(lx, y, rx, y, 1000));
+                } else {
+                    sprite.move(new LinePath(rx, y, lx, y, 1000));
+                }
+            }
+        });
+        sprite.move(new LinePath(lx, y, rx, y, 1000));
     }
 }
