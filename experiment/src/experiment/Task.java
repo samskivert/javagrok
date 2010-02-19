8ae1c21c02313dc7c264ef11cd931007198d9d1d
mdb <mdb@cs.washington.edu>
2010-02-19 14:29:32 -0800

    Factored out some non-intuitive and probably not relevant bits (creating a sprite from an image and creating and laying out a label glyph).

--- experiment/src/experiment/Task.java
+++ experiment/src/experiment/Task.java
@@ -3,14 +3,43 @@
 
 package experiment;
 
-import com.threerings.media.image.ImageManager;
+import java.awt.Color;
+import java.awt.Font;
+
+import com.samskivert.swing.Label;
 
 import com.threerings.media.MediaPanel;
+import com.threerings.media.sprite.ImageSprite;
+import com.threerings.media.image.ImageManager;
+import com.threerings.media.image.Mirage;
 
 /**
  * The base class for the experiment tasks.
  */
 public abstract class Task
 {
-    public abstract void init (ImageManager imgr, MediaPanel panel);
+    public void init (ImageManager imgr, MediaPanel panel) {
+        _imgr = imgr;
+        _panel = panel;
+        setup(_panel);
+    }
+
+    protected abstract void setup (MediaPanel panel);
+
+    protected ImageSprite createSprite (String imagePath)
+    {
+        return new ImageSprite(_imgr.getMirage(imagePath));
+    }
+
+    protected Label createLabel (String text)
+    {
+        Label label = new Label(text, Color.BLACK, LABEL_FONT);
+        label.layout(_panel);
+        return label;
+    }
+
+    protected ImageManager _imgr;
+    protected MediaPanel _panel;
+
+    protected static final Font LABEL_FONT = new Font("Dialog", Font.BOLD, 16);
 }
--- experiment/src/experiment/Task1.java
+++ experiment/src/experiment/Task1.java
@@ -3,9 +3,14 @@
 
 package experiment;
 
+import java.awt.Color;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import com.samskivert.swing.Label;
+
 import com.threerings.media.MediaPanel;
-import com.threerings.media.image.ImageManager;
-import com.threerings.media.image.Mirage;
+import com.threerings.media.animation.FloatingTextAnimation;
 import com.threerings.media.sprite.ImageSprite;
 import com.threerings.media.sprite.PathAdapter;
 import com.threerings.media.sprite.Sprite;
@@ -17,12 +22,12 @@ import com.threerings.media.util.Path;
  */
 public class Task1 extends Task
 {
-    public void init (ImageManager imgr, MediaPanel panel)
+    protected void setup (final MediaPanel panel)
     {
-        Mirage cartman = imgr.getMirage("cartman.png");
-        ImageSprite sprite = new ImageSprite(cartman);
-        panel.getSpriteManager().addSprite(sprite);
+        final ImageSprite sprite = createSprite("cartman.png");
+        panel.addSprite(sprite);
 
+        // moves the sprite back and forth across the display
         final int y = (panel.getHeight()-sprite.getHeight())/2;
         final int lx = 0, rx = panel.getWidth()-sprite.getWidth();
         sprite.addSpriteObserver(new PathAdapter() {
@@ -35,5 +40,14 @@ public class Task1 extends Task
             }
         });
         sprite.move(new LinePath(lx, y, rx, y, 1000));
+
+        // listens for mouse clicks and checks for intersection with the sprite
+        panel.addMouseListener(new MouseAdapter() {
+            public void mouseClicked (MouseEvent ev) {
+                String text = sprite.hitTest(ev.getX(), ev.getY()) ? "Ouch!" : "Ha ha!";
+                panel.addAnimation(
+                    new FloatingTextAnimation(createLabel(text), ev.getX(), ev.getY()));
+            }
+        });
     }
 }
