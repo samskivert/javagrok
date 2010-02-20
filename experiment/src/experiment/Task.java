f5a0f008fbf161e94fca5322b3a7aa78c3724436
reto <reto@ubuntu>
2010-02-19 16:15:02 -0800

    Merge with head of repository.

--- experiment/src/experiment/Task.java
+++ experiment/src/experiment/Task.java
@@ -3,14 +3,42 @@
 
 package experiment;
 
-import com.threerings.media.image.ImageManager;
+import java.awt.Color;
+import java.awt.Font;
+
+import com.samskivert.swing.Label;
 
 import com.threerings.media.MediaPanel;
+import com.threerings.media.image.ImageManager;
+import com.threerings.media.image.Mirage;
 
 /**
  * The base class for the experiment tasks.
  */
 public abstract class Task
 {
-    public abstract void init (ImageManager imgr, MediaPanel panel);
+    public void init (ImageManager imgr, MediaPanel panel) {
+        _imgr = imgr;
+        _panel = panel;
+        setup(_panel);
+    }
+
+    protected abstract void setup (MediaPanel panel);
+
+    protected Mirage getMirage (String imagePath)
+    {
+        return _imgr.getMirage(imagePath);
+    }
+
+    protected Label createLabel (String text)
+    {
+        Label label = new Label(text, Color.BLACK, LABEL_FONT);
+        label.layout(_panel);
+        return label;
+    }
+
+    protected ImageManager _imgr;
+    protected MediaPanel _panel;
+
+    protected static final Font LABEL_FONT = new Font("Dialog", Font.BOLD, 16);
 }
