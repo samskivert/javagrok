a33370474fb39ea92af5f38b8b196836692021c0
Michael Bayne <mdb@samskivert.com>
2010-03-04 12:04:27 -0800

    Adding the lisp interpreter. Reto and I think this is probably our safest bet for experimental tasks.

---
+++ uclisp/src/java/uclisp/Bdiv.java
@@ -0,0 +1,35 @@
+//
+// Bdiv - built in to perform division
+
+package uclisp;
+
+import java.util.Vector;
+
+public class Bdiv extends Function
+{
+    //
+    // Bdiv public member functions
+
+    public Object evaluate (Interpreter interp, Vector sexp)
+        throws RunTimeException
+    {
+        try {
+            Integer l = (Integer)interp.evaluateSExp(sexp.elementAt(0));
+            Integer r = (Integer)interp.evaluateSExp(sexp.elementAt(1));
+
+            if (l.intValue() == 0)
+                throw new RunTimeException("Division by zero.", sexp);
+
+            return new Integer(r.intValue()/l.intValue());
+
+        } catch (ClassCastException cce) {
+            throw new RunTimeException("Non-integer type used for " +
+                                       "subtraction expression.", sexp);
+        }
+    }
+
+    public int numArguments ()
+    {
+        return 2;
+    }
+}
---
+++ uclisp/src/java/uclisp/Scanner.java
@@ -0,0 +1,26 @@
+//
+// Scanner -
+
+package uclisp;
+
+import java.io.InputStream;
+import java.io.IOException;
+import java.io.StreamTokenizer;
+
+public class Scanner extends StreamTokenizer
+{
+    //
+    // Scanner public constructor
+
+    public Scanner (InputStream is)
+    {
+        super(is);
+
+        // initialize the stream tokenizer for lisp
+        commentChar(';');
+        parseNumbers();
+        quoteChar('"');
+        ordinaryChar('\'');
+        wordChars('_', '_');
+    }
+}
