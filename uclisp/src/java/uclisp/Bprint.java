e9ed58946e0e61af1a486c34c645aec2f9e6e1ee
mdb <mdb@cs.washington.edu>
2010-03-04 18:15:43 -0800

    Undoing changes to Bprint.java.

--- uclisp/src/java/uclisp/Bprint.java
+++ uclisp/src/java/uclisp/Bprint.java
@@ -3,11 +3,6 @@
 
 package uclisp;
 
-import org.javagrok.analysis.Retained;
-import org.javagrok.analysis.NotRetained;
-import org.javagrok.analysis.ExceptionProperty;
-import org.javagrok.analysis.LentThis;
-import org.javagrok.analysis.UniqueReturn;
 import java.io.PrintStream;
 
 import java.util.Vector;
@@ -17,34 +12,20 @@ public class Bprint extends Function
     //
     // Bprint public member functions
 
-    @LentThis
-    @UniqueReturn
-    public Object evaluate (@NotRetained Interpreter interp, @NotRetained Vector sexp)
+    public Object evaluate (Interpreter interp, Vector sexp)
         throws RunTimeException
     {
-        return evaluate(interp, sexp, "");
-    }
-    
-    // Special evaluate taking a seperator
-    
-    private Object evaluate(Interpreter interp, Vector sexp, String sep) throws RunTimeException {
-    	for (int i = 0; i < sexp.size(); i++) {
+        for (int i = 0; i < sexp.size(); i++) {
             printValue(interp, sexp.elementAt(i));
-            if (i < sexp.size()-1) {
-            	_out.print(sep);
-            }
         }
         _out.flush();
         return new Nil();
-	}
-    
+    }
 
     //
     // Bprint protected member functions
 
-    @LentThis
-    @ExceptionProperty(throwsWhen="<br>RunTimeException when !(v instanceof Vector)", exceptionsThrown="explicitly throws RunTimeException")
-    public void printValue (@NotRetained Interpreter interp, @Retained Object v)
+    public void printValue (Interpreter interp, Object v)
         throws RunTimeException
     {
         if ((v instanceof String) ||
@@ -55,9 +36,7 @@ public class Bprint extends Function
             _out.print("nil");
 
         } else if (v instanceof List) {
-        	_out.print("(");
-            evaluate(interp, (Vector)v, " ");
-            _out.print(")");
+            evaluate(interp, (Vector)v);
 
         } else if (v instanceof Name) {
             printValue(interp, interp.evaluateSExp(v));
@@ -71,12 +50,10 @@ public class Bprint extends Function
         }
     }
 
-	//
+    //
     // Bprint public static member functions
 
-
-
-	public static void setOutputStream (@Retained PrintStream out)
+    public static void setOutputStream (PrintStream out)
     {
         _out = out;
     }
