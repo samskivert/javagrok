990d609fc819aab949a71a72846f34d64d5284d7
mdb <mdb@cs.washington.edu>
2010-03-05 14:41:27 -0800

    Nixed unused import.

--- uclisp/src/java/uclisp/Interpreter.java
+++ uclisp/src/java/uclisp/Interpreter.java
@@ -6,7 +6,6 @@ package uclisp;
 import java.io.FileReader;
 import java.io.IOException;
 
-import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.NoSuchElementException;
