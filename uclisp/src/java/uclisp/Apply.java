d535b325541d90f9e7f34b6212bb66b9e86281f6
mdb <mdb@cs.washington.edu>
2010-03-05 00:28:56 -0800

    Missed a spot.

---
+++ uclisp/src/java/uclisp/Apply.java
@@ -0,0 +1,14 @@
+//
+// Apply - boxes a list that represents function application
+
+package uclisp;
+
+public class Apply
+{
+    public final List sexp;
+
+    public Apply (List sexp)
+    {
+        this.sexp = sexp;
+    }
+}
