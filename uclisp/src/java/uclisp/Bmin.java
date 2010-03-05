b418066b857ae98e579d17b65c951d81566197d2
mdb <mdb@cs.washington.edu>
2010-03-04 18:18:57 -0800

    Being a formatting Nazi. Also nixed imports. They can add those if and when they need them.

--- uclisp/src/java/uclisp/Bmin.java
+++ uclisp/src/java/uclisp/Bmin.java
@@ -1,32 +1,32 @@
+//
+// Bmin - a builtin function to return the least of its arguments
+
 package uclisp;
 
-import java.util.Vector;
-
-public class Bmin extends Function {
-	
-	/*
-	 * TASK 1
-	 * ======
-	 * 
-	 * Hello and welcome to the first task. I hope you 
-	 * are all set and have received cookies and a drink to
-	 * be ready to complete the tasks. :)
-	 * 
-	 * You're first task is to get comfortable with the framework.
-	 * To do so you will be implementing the minimum function of
-	 * Lisp. Minimum takes one or more integer arguments and returns
-	 * the smallest.
-	 * 
-	 * examples: (min 3) --> 3
-	 *           (min 3 1) --> 1
-	 *           (min 3 -1 1) --> -1
-	 *           
-	 * Hint: See other functions and how they are implemented to
-	 * get a clue what to do!
-	 * 
-	 * At the end you should be able to run: ant min
-	 * Which will run the lisp/min.lisp program.
-	 * 
-	 */
-	
+public class Bmin extends Function
+{
+    /*
+     * TASK 1
+     * ======
+     * 
+     * Hello and welcome to the first task. I hope you 
+     * are all set and have received cookies and a drink to
+     * be ready to complete the tasks. :)
+     * 
+     * You're first task is to get comfortable with the framework.
+     * To do so you will be implementing the minimum function of
+     * Lisp. Minimum takes one or more integer arguments and returns
+     * the smallest.
+     * 
+     * examples: (min 3) --> 3
+     *           (min 3 1) --> 1
+     *           (min 3 -1 1) --> -1
+     *           
+     * Hint: See other functions and how they are implemented to
+     * get a clue what to do!
+     * 
+     * At the end you should be able to run: ant min
+     * Which will run the lisp/min.lisp program.
+     * 
+     */
 }
