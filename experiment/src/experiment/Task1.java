891bee5d9712e3b40ca049c6ddf9b2b7f84b4937
reto <reto@localhost>
2010-03-07 22:58:35 -0800

    Updated the first task to give some hints.

--- experiment/src/experiment/Task1.java
+++ experiment/src/experiment/Task1.java
@@ -12,6 +12,14 @@ public class Task1 extends Task
 {
     protected void setup (final MediaPanel panel)
     {
+        // Hello and welcome to the first task. I hope you are all set and have
+        // received cookies and a drink to be ready to complete the tasks. :)
+    	// You should also have a questionnaire and experiment overview document.
+    	
+    	// Remember: This experiment does not assess you, it assess our tool.
+    	// Something else: You do not have to complete all the tasks. It is totally okay to 
+    	// stop after an hour.
+    	
         // Step 1: Create an ImageSprite with the image "cartman.png" and add it to the MediaPanel
         // supplied as an argument.
 
@@ -19,9 +27,19 @@ public class Task1 extends Task
         // class of this task skeleton) to obtain the image that you will supply to the
         // ImageSprite.
 
-        // Step 2: Move the sprite to the center of the window. The coordinates are (0, 0) in the
-        // upper left of the window and both the sprite and the panel have methods to obtain their
-        // width and height.
+        // Step 2: Move the ImageSprite on a Path back and forth across the window. You will want
+        // to use the LinePath class to move the sprite. The LinePath will only move the Sprite
+        // from one point to another, so you will have to add a PathObserver to the Sprite to
+        // receive notification when the Sprite reaches the end of its path. At that point, set the
+        // sprite on a new path going back in the opposite direction, so that you achieve an
+        // oscilating motion back and forth across the window.
+    	
+    	// Hint: The top left corner of the panel has the coordinates (0,0). The top right
+    	// corner has the coordinates (0, panel's width) and the bottom right corner has
+    	// the coordinates (panel's height, panel's width).
+    	
+    	// Hint 2: The actual movement has to be invoked on the sprite. Not the panel.
+    	
 
         // Step 3: Move the ImageSprite on a Path back and forth across the window. You will want
         // to use the LinePath class to move the sprite. The path should be added to the sprite
